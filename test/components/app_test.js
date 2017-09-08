3dac1433a1e7cc195f34d54b86af6d3f3e315971
Aki Gao <xag9bb@virginia.edu>
2017-09-08 00:00:56 -0400

    complete section 2, part 5

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,16 @@
-import {renderComponent, expect} from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
 // use describe to group together similar tests
 // specify string inside describe for testing output report
-describe('App')
-
-// use 'it' to test a single attribute of a target
-// use 'it' to group one single test
-// used ONLY for the testing output report
-it('shows the correct text')
-
-// use expect to make an 'assertion' about a target or what you are testing
-expect()
+describe('App', () => {
+  // use 'it' to test a single attribute of a target
+  // use 'it' to group one single test
+  // used ONLY for the testing output report
+  it('shows the correct text', () => {
+    //create an instance of app
+    const component = renderComponent(App);
+    // use expect to make an 'assertion' about a target or what you are testing
+    expect(component).to.contain('React simple starter');
+  });
+});
